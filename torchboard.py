9aa2b8a3d2e39f5594a19ef56d1aeb7797d9b0f4
root <root@DESKTOP-F9NBMCF.localdomain>
2018-03-19 01:17:59 -0700

    visualization works with jumbled logs

--- torchboard.py
+++ torchboard.py
@@ -5,6 +5,8 @@ import matplotlib
 matplotlib.use("Agg") 
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+sns.set_style("ticks")
 
 # graph loss over time
 # parsing log file to extract loss data
@@ -51,8 +53,8 @@ if __name__ == '__main__':
 				num_epochs = int(f[j+1].split("/")[1])
 
 	# print("here are ", len(epochs_indices))
-	train_loss = [-1 for i in range(num_epochs+1)]
-	eval_loss = [-1 for i in range(num_epochs+1)]
+	train_loss = [0 for i in range(num_epochs+1)]
+	eval_loss = [0 for i in range(num_epochs+1)]
 
 	# extract loss
 	for j in range(0, len(epochs_indices)):
@@ -66,27 +68,53 @@ if __name__ == '__main__':
 		if j+1 < len(epochs_indices):
 			upper_bound = epochs_indices[j+1]
 
+		# log file is a bit jumbled...
+		num_train = 0
 		for k in range(epochs_indices[j], upper_bound):
 			if f[k] == "Train":
-				train_loss[curr_epoch] = f[k+5]
-				# print("Epoch: ", curr_epoch, " train loss: ", f[k+5])
+				# first train belonged to the previous epoch which appeared in the log (also not in chronological epoch order RIP WTF)
+				prev_epoch = int(f[epochs_indices[j-1]+1].split("/")[0])
+				
+				if num_train == 1 and prev_epoch >= 1:
+					train_loss[prev_epoch] = first_train_loss
+					# print(prev_epoch)
+
+				# if there are more than one train losses in a epoch, the first one actually belongs to the previous epoch
+				first_train_loss = float(f[k+5])
+				train_loss[curr_epoch] = float(f[k+5])
+				num_train += 1
+
+			# strangely, eval losses are not scrambled like the train losses
 			if f[k] == "Eval":
-				eval_loss[curr_epoch] = f[k+5]
+				eval_loss[curr_epoch] = float(f[k+5])
 
-		# train_loss.append(f[epochs_indices[j]+11])
-		# eval_loss.append(f[epochs_indices[j]+22])
+	# test to see epochs map correctly to loss
+	for j in range(1, num_epochs+1):
+		print("Epoch: ", j, " train loss: ", eval_loss[j])
 
-		# print(f[epochs_indices[j]+11])
+	print(max(train_loss))
+	print(max(eval_loss))
 
-		# train_loss[curr_epoch] = f[epochs_indices[j]+11]
-		# eval_loss[curr_epoch] = f[epochs_indices[j]+22]
-
-	print(len(train_loss))
-	print(len(eval_loss))
-
+	# train plot
+	plt.figure(0)
 	plt.plot(np.arange(num_epochs+1)[1:], train_loss[1:], 'ro') # since train epoch indexing starts at 1
+	plt.yticks(np.arange(min(train_loss), max(train_loss)+0.1, 0.1))
+	plt.savefig("train.png")
+
+	# eval plot
+	plt.figure(1)
+	plt.plot(np.arange(num_epochs+1)[1:], eval_loss[1:], 'ro') # since train epoch indexing starts at 1
+	plt.yticks(np.arange(min(eval_loss), max(eval_loss), 1))
 	plt.show()
-	plt.savefig("testlol.png")
+	plt.savefig("eval.png")
+
+	# together
+	plt.figure(2)
+	plt.plot(np.arange(num_epochs+1)[1:], train_loss[1:], 'r--', np.arange(num_epochs+1)[1:], eval_loss[1:], 'bs')
+	plt.yticks(np.arange(min(min(eval_loss), min(train_loss)), max(max(eval_loss), max(train_loss)), 1))
+	plt.legend()
+	plt.show()
+	plt.savefig("together.png")
 
 			
 
